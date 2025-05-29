8fbb65854ee53bc5b11f337977af93087f640c12
AADHISESHAN K S <83263942+aadhiseshanks@users.noreply.github.com>
2025-05-29 09:00:37 +0530

    .

--- PATTERNS/pattern.c
+++ PATTERNS/pattern.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+
 int main() {
     int num;
     scanf("%d", &num);
-    printf("Increasing Star\n");
+    printf("Increasing Star Pattern\n");
     for(int i=1; i<=num; i++) {
         for(int j=1; j<=i; j++) {
             printf("*");
         }
         printf("\n");
     }
-    printf("\nDecreasing Star\n");
+    
+    printf("\nDecreasing Star Pattern\n");
     for(int i=1; i<=num; i++) {
         for(int j=i; j<=num; j++) {
             printf("*");
@@ -17,12 +19,23 @@ int main() {
         printf("\n");
     }
     
-    printf("\nRight Angle Triangle\n");
+    printf("\nRight Angle Triangle Pattern\n");
     for(int i=1; i<=num; i++) {
         for(int j=1; j<=i; j++) {
             printf(" ");
         }
         for(int j=i; j<=num; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    printf("\nRight Angle Triangle Pattern\n");
+    for(int i=1; i<=num; i++) {
+        for(int j=i; j<=num; j++) {
+            printf(" ");
+        }
+        for(int j=1; j<=i; j++) {
             printf("*");
         }
         printf("\n");
@@ -33,10 +46,10 @@ int main() {
         for(int j=i; j<=num; j++) {
             printf(" ");
         }
-        for(int j=1; j<=i; j++) {
+        for(int j=1; j<i; j++) {
             printf("*");
         }
-        for(int j=1; j<i; j++) {
+        for(int j=1; j<=i; j++) {
             printf("*");
         }
         printf("\n");
@@ -47,17 +60,17 @@ int main() {
         for(int j=1; j<=i; j++) {
             printf(" ");
         }
-        for(int j=i; j<=num; j++) {
+        for(int j=i; j<num; j++) {
             printf("*");
         }
-        for(int j=i; j<num; j++) {
+        for(int j=i; j<=num; j++) {
             printf("*");
         }
         printf("\n");
     }
     
     printf("\nDiamond Pattern\n");
-    for(int i=1; i<num; i++) {
+    for(int i=1; i<=num-1; i++) {
         for(int j=i; j<=num; j++) {
             printf(" ");
         }
@@ -72,30 +85,12 @@ int main() {
     for(int i=1; i<=num; i++) {
         for(int j=1; j<=i; j++) {
             printf(" ");
-       }
+        }
         for(int j=i; j<num; j++) {
             printf("*");
         }
         for(int j=i; j<=num; j++) {
             printf("*");
-        }
-        printf("\n");
-    }
-    
-    printf("\nHollow Increasing Pattern\n");
-    for(int i=1; i<=num; i++) {
-        for(int j=1; j<=i; j++) {
-            if(j==1 || i == num || i == j) printf("*");
-            else printf(" ");
-        }
-        printf("\n");
-    }
-    
-    printf("\nHollow Decreasing Pattern\n");
-    for(int i=1; i<=num; i++) {
-        for(int j=i; j<=num; j++) {
-            if(i== 1 || j == num || i == j) printf("*");
-            else printf(" ");
         }
         printf("\n");
     }
@@ -118,7 +113,7 @@ int main() {
     }
     
     printf("\nButterfly Pattern\n");
-    for(int i=1; i<=num; i++) {
+    for(int i=1; i<=num-1; i++) {
         for(int j=1; j<=i; j++) {
             printf("*");
         }
@@ -133,7 +128,6 @@ int main() {
         }
         printf("\n");
     }
-    
     for(int i=1; i<=num; i++) {
         for(int j=i; j<=num; j++) {
             printf("*");
@@ -146,6 +140,86 @@ int main() {
         }
         for(int j=i; j<=num; j++) {
             printf("*");
+        }
+        printf("\n");
+    }
+    
+    printf("\nHollow Increasing Pattern\n");
+    for(int i=1; i<=num; i++) {
+        for(int j=1; j<=i; j++) {
+            if(j==1 || i==num || i==j) printf("*");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    
+    printf("\nHollow Decreasing Pattern\n");
+    for(int i=1; i<=num; i++) {
+        for(int j=i; j<=num; j++) {
+            if(i==1 || j==num || j==i) printf("*");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    
+    printf("\nHollow Hill Pattern\n");
+    for(int i=1; i<=num; i++) {
+        for(int j=i; j<=num; j++) {
+            printf(" ");
+        }
+        for(int j=1; j<i; j++) {
+            if(j==1||i==num) printf("*");
+            else printf(" ");
+        }
+        for(int j=1; j<=i; j++) {
+            if(j==i || i==num) printf("*");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    
+    printf("\nHollow Reversed Hill Pattern\n");
+    for(int i=1; i<=num; i++) {
+        for(int j=1; j<=i; j++) {
+            printf(" ");
+        }
+        for(int j=i; j<num; j++) {
+            if(i==1 || j==i) printf("*");
+            else printf(" ");
+        }
+        for(int j=i; j<=num; j++) {
+            if(i==1 || j==num)  printf("*");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    
+    printf("\nHollow Diamond Pattern\n");
+    for(int i=1; i<=num-1; i++) {
+        for(int j=i; j<=num; j++) {
+            printf(" ");
+        }
+        for(int j=1; j<i; j++) {
+            if(j==1) printf("*");
+            else printf(" ");
+        }
+        for(int j=1; j<=i; j++) {
+            if(j==i) printf("*");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    for(int i=1; i<=num; i++) {
+        for(int j=1; j<=i; j++) {
+            printf(" ");
+        }
+        for(int j=i; j<num; j++) {
+            if(j==i) printf("*");
+            else printf(" ");
+        }
+        for(int j=i; j<=num; j++) {
+            if(j==num) printf("*");
+            else printf(" ");
         }
         printf("\n");
     }
